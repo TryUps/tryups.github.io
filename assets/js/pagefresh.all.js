f236cc5096d41525ea9e14441774055dae18a570
Arilson Ferreira <arilsonbrj@gmail.com>
2019-08-17 22:42:49 -0300

    Updated PageFresh

--- assets/js/pagefresh.all.js
+++ assets/js/pagefresh.all.js
@@ -3,68 +3,63 @@
  * TryUps Fresh Pages
  * Hashtag pagination very easy.
  */
-(function(){
-    const fresh = function({el, settings}){
-        var el = el || "body";
-        this.el = document.querySelector(el);
-        this.settings = settings | {"module":false,"folder":"/pages/"};
+const fresh = function({el, settings}){
+    var el = el || "body";
+    this.el = document.querySelector(el);
+    this.settings = settings | {"module":false,"folder":"/pages/"};
+}
+fresh.prototype.load = function(url){
+    if(Array.isArray(url) == true){
+        const request = async() => {
+            const folder = this.settings.folder || "/pages/";
+            const results = await Promise.all(url.map((url) => fetch(folder + url + ".html").then((r) => r.text())));
+            return results;
+        }
+        request().then(data => this.el.innerHTML = data.toString());
+    }else{
+        const request = async () => {
+            let response = await fetch("pages/" + url + ".html");
+            var data = await response.text();
+            return data;
+        }
+        var lock = [];
+        request().then(data => {
+            this.el.innerHTML = data;
+        });
     }
-    fresh.prototype.load = function(url){
-        if(Array.isArray(url) == true){
-            const request = async() => {
-                const folder = this.settings.folder || "/pages/";
-                const results = await Promise.all(url.map((url) => fetch(folder + url + ".html").then((r) => r.text())));
-                return results;
-            }
-            request().then(data => this.el.innerHTML = data.toString());
+}
+
+fresh.prototype.go = function(to){
+    var to = to || "blank";
+    let url = location.hash.slice(3);
+    if(url == ""){
+        location.hash =  this.settings.home || "#!/home";
+        url = this.settings.home.slice(3) || "home";
+    }
+    this.load(url);
+    this.setTitle();
+}
+
+fresh.prototype.setTitle = function(){
+    setTimeout(function(){
+        let pgTitle = document.querySelector("meta[name='fresh']");
+        if(!pgTitle){
+            pgTitle == "Error!";
         }else{
-            const request = async () => {
-                let response = await fetch("pages/" + url + ".html");
-                var data = await response.text();
-                return data;
-            }
-            var lock = [];
-            request().then(data => {
-
-                this.el.innerHTML = data;
-            
-            });
+            pgTitle = pgTitle.getAttribute("value");
         }
-    }
-
-    fresh.prototype.go = function(to){
-        var to = to || "blank";
-        let url = location.hash.slice(3);
-        if(url == ""){
-            location.hash =  this.settings.home || "#!/home";
-            url = this.settings.home.slice(3) || "home";
+        if(pgTitle == null || pgTitle == ""){
+            pgTitle = "Error!";
         }
+        document.title = document.title + " – " + pgTitle;
+    },100);
+}
 
-        this.load(url);
-        this.setTitle();
-    }
+window.addEventListener('hashchange', function() {
+    fresh.go();
+}, false);
 
-    fresh.prototype.setTitle = function(){
-        setTimeout(function(){
-            let pgTitle = document.querySelector("meta[name='fresh']");
-            if(!pgTitle){
-                pgTitle == "Error!";
-            }else{
-                pgTitle = pgTitle.getAttribute("value");
-            }
-            if(pgTitle == null || pgTitle == ""){
-                pgTitle = "Error!";
-            }
-            document.title = document.title + " – " + pgTitle;
-        },100);
-    }
+window.onhashchange = fresh.go();
 
-    window.addEventListener('hashchange', function() {
-        fresh.go();
-    }, false);
-
-    window.onhashchange = fresh.go();
-
-    window.fresh = fresh;
-    module.exports = fresh;
-})();
+window.fresh = fresh;
+module.exports = fresh;
